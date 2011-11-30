139c1c33fee42ebbccb47f2ee699050e0d5c118e
Roger Zanoni <roger.zanoni@openbossa.org>
2011-11-30 15:43:39 -0400

    Add updateScript property
    This property contains the update logic for this item.
    
    Signed-off-by: Roger Zanoni <roger.zanoni@openbossa.org>

--- src/gameitem.cpp
+++ src/gameitem.cpp
@@ -1,10 +1,32 @@
 #include "gameitem.h"
 
+#include <QDeclarativeExpression>
+
 GameItem::GameItem(QQuickItem *parent)
-    : QQuickItem(parent)
+    : QQuickItem(parent),
+      m_expression(0)
 {
 }
 
 void GameItem::update(long delta)
 {
+    if (m_expression)
+        m_expression->evaluate();
+}
+
+QDeclarativeScriptString GameItem::updateScript() const
+{
+    return m_updateScript;
+}
+
+void GameItem::setUpdateScript(QDeclarativeScriptString updateScript)
+{
+    m_updateScript = updateScript;
+
+    if (m_expression)
+        delete m_expression;
+
+    m_expression = new QDeclarativeExpression(m_updateScript);
+
+    emit updateScriptChanged();
 }
--- src/gameitem.h
+++ src/gameitem.h
@@ -2,16 +2,31 @@
 #define _GAMEITEM_H_
 
 #include <QtDeclarative>
+#include <QDeclarativeScriptString>
+
+class QDeclarativeExpression;
 
 class GameItem : public QQuickItem
 {
     Q_OBJECT
 
+    Q_PROPERTY(QDeclarativeScriptString updateScript READ updateScript WRITE setUpdateScript NOTIFY updateScriptChanged)
+
 public:
     GameItem(QQuickItem *parent = 0);
 
+    QDeclarativeScriptString updateScript() const;
+    void setUpdateScript(QDeclarativeScriptString updateScript);
+
 public slots:
     virtual void update(long delta);
+
+signals:
+    void updateScriptChanged();
+
+private:
+    QDeclarativeScriptString m_updateScript;
+    QDeclarativeExpression *m_expression;
 };
 
 #endif /* _GAMEITEM_H_ */
