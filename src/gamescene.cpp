1fa5d5b912115f85774a1f0915504eabc62c51c3
Roger Zanoni <roger.zanoni@openbossa.org>
2011-11-30 14:39:15 -0400

    Add running property
    This property is used to prevent gameItems to be updated
    
    Signed-off-by: Roger Zanoni <roger.zanoni@openbossa.org>

--- src/gamescene.cpp
+++ src/gamescene.cpp
@@ -12,8 +12,23 @@ QDeclarativeListProperty<GameItem> GameScene::gameItems()
 
 void GameScene::update(long delta)
 {
+    if (!m_running) // TODO: stop Qt animations as well
+        return;
+
     GameItem *item;
 
     foreach (item, m_gameItems)
         item->update(delta);
+}
+
+bool GameScene::running() const
+{
+    return m_running;
+}
+
+void GameScene::setRunning(bool running)
+{
+    m_running = running;
+
+    emit runningChanged();
 }
--- src/gamescene.h
+++ src/gamescene.h
@@ -11,17 +11,25 @@ class GameScene : public QQuickItem
     Q_OBJECT
 
     Q_PROPERTY(QDeclarativeListProperty<GameItem> gameItems READ gameItems)
+    Q_PROPERTY(bool running READ running WRITE setRunning NOTIFY runningChanged)
 
 public:
     GameScene(QQuickItem *parent = 0);
 
     QDeclarativeListProperty<GameItem> gameItems();
 
+    bool running() const;
+    void setRunning(bool running);
+
 public slots:
     void update(long delta);
 
+signals:
+    void runningChanged();
+
 private:
     QList<GameItem *> m_gameItems;
+    bool m_running;
 };
 
 #endif /* _GAMESCENE_H_ */
