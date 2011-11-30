fcd4bbbeebeee8bcb0eb3876ff0f6c08552a1ac3
Roger Zanoni <roger.zanoni@openbossa.org>
2011-11-30 13:15:02 -0400

    Add currentScene property
    When this property changes, the update signal is disconnected from previous scene and only the current scene will receive updates.
    
    Signed-off-by: Roger Zanoni <roger.zanoni@openbossa.org>

--- src/quasigame.cpp
+++ src/quasigame.cpp
@@ -1,6 +1,24 @@
 #include "quasigame.h"
+#include "gamescene.h"
 
 QuasiGame::QuasiGame(QQuickItem *parent)
-    : QQuickItem(parent)
+    : QQuickItem(parent),
+      m_currentScene(0)
 {
+}
+
+GameScene *QuasiGame::currentScene() const {
+    return m_currentScene;
+}
+
+void QuasiGame::setCurrentScene(GameScene *currentScene) {
+    if (m_currentScene)
+        disconnect(SIGNAL(update(long)));
+
+    m_currentScene = currentScene;
+
+    if (m_currentScene)
+        connect(this, SIGNAL(update(long)), m_currentScene, SLOT(update(long)));
+
+    emit currentSceneChanged();
 }
--- src/quasigame.h
+++ src/quasigame.h
@@ -3,15 +3,26 @@
 
 #include <QtDeclarative>
 
+class GameScene;
+
 class QuasiGame : public QQuickItem
 {
     Q_OBJECT
 
+    Q_PROPERTY(GameScene *currentScene READ currentScene WRITE setCurrentScene NOTIFY currentSceneChanged)
+
 public:
     QuasiGame(QQuickItem *parent = 0);
 
+    GameScene *currentScene() const;
+    void setCurrentScene(GameScene *currentScene);
+
 signals:
     void update(long delta);
+    void currentSceneChanged();
+
+private:
+    GameScene *m_currentScene;
 };
 
 #endif /* _QUASIGAME_H_ */
