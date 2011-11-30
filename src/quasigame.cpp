0d136b97f446f6c7777bc06be7e7093621a37014
Roger Zanoni <roger.zanoni@openbossa.org>
2011-11-30 14:22:21 -0400

    Add update timer
    Signed-off-by: Roger Zanoni <roger.zanoni@openbossa.org>

--- src/quasigame.cpp
+++ src/quasigame.cpp
@@ -3,8 +3,12 @@
 
 QuasiGame::QuasiGame(QQuickItem *parent)
     : QQuickItem(parent),
-      m_currentScene(0)
+      m_currentScene(0),
+      m_fps(DEFAULT_FPS)
 {
+    connect(&m_updateTimer, SIGNAL(timeout()), this, SLOT(update()));
+    m_gameTime.start();
+    m_updateTimer.start(1000 / m_fps);
 }
 
 GameScene *QuasiGame::currentScene() const {
@@ -21,4 +25,22 @@ void QuasiGame::setCurrentScene(GameScene *currentScene) {
         connect(this, SIGNAL(update(long)), m_currentScene, SLOT(update(long)));
 
     emit currentSceneChanged();
+}
+
+int QuasiGame::fps() const
+{
+    return m_fps;
+}
+
+void QuasiGame::setFps(int fps)
+{
+    m_fps = fps;
+
+    emit fpsChanged();
+}
+
+void QuasiGame::update()
+{
+    long elapsedTime = m_gameTime.restart();
+    emit update(elapsedTime);
 }
--- src/quasigame.h
+++ src/quasigame.h
@@ -1,6 +1,8 @@
 #ifndef _QUASIGAME_H_
 #define _QUASIGAME_H_
 
+#include <QtCore/QTimer>
+#include <QtCore/QTime>
 #include <QtDeclarative>
 
 class GameScene;
@@ -9,7 +11,10 @@ class QuasiGame : public QQuickItem
 {
     Q_OBJECT
 
+    enum { DEFAULT_FPS = 60 };
+
     Q_PROPERTY(GameScene *currentScene READ currentScene WRITE setCurrentScene NOTIFY currentSceneChanged)
+    Q_PROPERTY(int fps READ fps WRITE setFps NOTIFY fpsChanged)
 
 public:
     QuasiGame(QQuickItem *parent = 0);
@@ -17,12 +22,22 @@ public:
     GameScene *currentScene() const;
     void setCurrentScene(GameScene *currentScene);
 
+    int fps() const;
+    void setFps(int fps);
+
+public slots:
+    void update();
+
 signals:
     void update(long delta);
     void currentSceneChanged();
+    void fpsChanged();
 
 private:
     GameScene *m_currentScene;
+    QTimer m_updateTimer;
+    QTime m_gameTime;
+    int m_fps;
 };
 
 #endif /* _QUASIGAME_H_ */
