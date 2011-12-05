1ab62b060ddd12f4f1a6622bdcfff265d6f2c00d
Roger Zanoni <roger.zanoni@openbossa.org>
2011-12-05 15:24:37 -0400

    Add updateInterval property

--- src/gameitem.cpp
+++ src/gameitem.cpp
@@ -4,14 +4,19 @@
 
 GameItem::GameItem(QQuickItem *parent)
     : QQuickItem(parent),
-      m_expression(0)
+      m_expression(0),
+      m_updateInterval(0)
 {
 }
 
 void GameItem::update(long delta)
 {
-    if (m_expression)
-        m_expression->evaluate();
+    if (m_updateInterval && m_updateTime.elapsed() >= m_updateInterval
+        || !m_updateInterval) {
+        m_updateTime.restart();
+        if (m_expression)
+            m_expression->evaluate();
+    }
 }
 
 QDeclarativeScriptString GameItem::updateScript() const
@@ -27,6 +32,18 @@ void GameItem::setUpdateScript(QDeclarativeScriptString updateScript)
         delete m_expression;
 
     m_expression = new QDeclarativeExpression(m_updateScript.context(), m_updateScript.scopeObject(), m_updateScript.script());
+
+    emit updateScriptChanged();
+}
+
+int GameItem::updateInterval() const
+{
+    return m_updateInterval;
+}
+
+void GameItem::setUpdateInterval(int updateInterval)
+{
+    m_updateInterval = updateInterval;
 
     emit updateScriptChanged();
 }
--- src/gameitem.h
+++ src/gameitem.h
@@ -1,6 +1,7 @@
 #ifndef _GAMEITEM_H_
 #define _GAMEITEM_H_
 
+#include <QTime>
 #include <QtDeclarative>
 #include <QDeclarativeScriptString>
 
@@ -11,6 +12,7 @@ class GameItem : public QQuickItem
     Q_OBJECT
 
     Q_PROPERTY(QDeclarativeScriptString updateScript READ updateScript WRITE setUpdateScript NOTIFY updateScriptChanged)
+    Q_PROPERTY(int updateInterval READ updateInterval WRITE setUpdateInterval NOTIFY updateIntervalChanged)
 
 public:
     GameItem(QQuickItem *parent = 0);
@@ -18,15 +20,21 @@ public:
     QDeclarativeScriptString updateScript() const;
     void setUpdateScript(QDeclarativeScriptString updateScript);
 
+    int updateInterval() const;
+    void setUpdateInterval(int updateInterval);
+
 public slots:
     virtual void update(long delta);
 
 signals:
     void updateScriptChanged();
+    void updateIntervalChanged();
 
 private:
     QDeclarativeScriptString m_updateScript;
     QDeclarativeExpression *m_expression;
+    int m_updateInterval;
+    QTime m_updateTime;
 };
 
 #endif /* _GAMEITEM_H_ */
